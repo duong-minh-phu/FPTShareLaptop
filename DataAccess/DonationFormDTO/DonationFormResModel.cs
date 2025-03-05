d9fb2ccf71140f3c1d60fcde1d97c4ee85ee92c3
Khang Nguyễn <nigo4666@gmail.com>
2025-03-05 20:59:49 +0700

    [feat] Donation Form

---
+++ DataAccess/DonationFormDTO/DonationFormResModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Enums;
+
+namespace DataAccess.DonationFormDTO
+{
+    public class DonationFormResModel
+    {
+        public int DonationFormId { get; set; }
+
+        public int SponsorId { get; set; }
+
+        public string ItemName { get; set; } = string.Empty;
+        public string? ItemDescription { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Status { get; set; }
+    }
+}
