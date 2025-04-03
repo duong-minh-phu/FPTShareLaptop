c02096ab8fb78aadc8e9d1dc723881cae8eda0af
khang <nigo4666@gmail.com>
2025-04-03 17:08:10 +0700

    [feat] Verify Student

---
+++ Service/IService/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.StudentDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.IService
+{
+    public interface IStudentService
+    {
+        Task<StudentResModel> VerifyStudent(string token, StudentReqModel request);
+        Task<string> SaveImage(IFormFile image);
+    }
+}
