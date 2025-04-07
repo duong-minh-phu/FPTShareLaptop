9326f244f02d765be6213b16fdba9c4bf8d56ac5
khang <nigo4666@gmail.com>
2025-04-07 21:35:25 +0700

    [feat] Add Image to DonateForm and fix UpdateDonateForm

--- DataAccess/DonateFormDTO/CreateDonateFormReqModel.cs
+++ DataAccess/DonateFormDTO/CreateDonateFormReqModel.cs
@@ -5,17 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessObjects.Enums;
+using Microsoft.AspNetCore.Http;
 
 namespace DataAccess.DonationFormDTO
 {
     public class CreateDonateFormReqModel
     {
+        [Required(ErrorMessage = "SponsorId is required")]
+        public int SponsorId {  get; set; }
         [Required(ErrorMessage = "ItemName is required")]
         public string ItemName { get; set; } = string.Empty;
         [Required(ErrorMessage = "ItemDescription is required")]
         public string ItemDescription { get; set; }
         [Required(ErrorMessage = "Quantity is required")]
-        public int Quantity { get; set; }  
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Image is required")]
+        public IFormFile ImageDonateForm { get; set; }
       
     }
 }
--- Service/Service/DonateFormService.cs
+++ Service/Service/DonateFormService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using BusinessObjects.Enums;
 using BusinessObjects.Models;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
 using DataAccess.DonationFormDTO;
 using Service.IService;
 using Service.Utils.CustomException;
@@ -10,10 +12,14 @@ namespace Service.Service
     public class DonationFormService : IDonateFormService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly Cloudinary _cloudinary;
+        private readonly IJWTService _jwtService;
 
-        public DonationFormService(IUnitOfWork unitOfWork)
+        public DonationFormService(IUnitOfWork unitOfWork, Cloudinary cloudinary, IJWTService jWTService)
         {
             _unitOfWork = unitOfWork;
+            _cloudinary = cloudinary;
+            _jwtService = jWTService;
         }
 
         public async Task<List<DonateFormResModel>> GetAllDonationsAsync()
@@ -49,31 +55,62 @@ namespace Service.Service
         }
 
 
-        public async Task CreateDonationAsync(CreateDonateFormReqModel request)
+        public async Task<DonateFormResModel> CreateDonationAsync(CreateDonateFormReqModel request)
         {
+            using var stream = request.ImageDonateForm.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(request.ImageDonateForm.FileName, stream),
+                Folder = "form_images"
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            if (uploadResult.Error != null)
+            {
+                throw new ApiException(HttpStatusCode.InternalServerError, uploadResult.Error.Message);
+            }
+
+            string imageFormUrl = uploadResult.SecureUrl.ToString();
+
             var newDonate = new DonateForm
             {
+                UserId = request.SponsorId,
                 ItemName = request.ItemName,
                 ItemDescription = request.ItemDescription,
                 DonateQuantity = request.Quantity,
+                ImageDonateForm = imageFormUrl,
                 Status = DonateStatus.Pending.ToString(),
                 CreatedDate = DateTime.UtcNow
             };
 
             await _unitOfWork.DonateForm.AddAsync(newDonate);
             await _unitOfWork.SaveAsync();
+            
+            return new DonateFormResModel
+            {
+                DonationFormId = newDonate.DonateFormId,
+                SponsorId = newDonate.UserId,
+                ItemName = newDonate.ItemName,
+                ItemDescription = newDonate.ItemDescription,
+                ImageDonateForm = newDonate.ImageDonateForm,
+                Quantity = newDonate.DonateQuantity,
+                CreatedAt = newDonate.CreatedDate,
+                Status = newDonate.Status
+            };
         }
 
 
-        public async Task UpdateDonationAsync(int id, UpdateDonateFormReqModel request)
+        public async Task UpdateDonationAsync(string token, int id, UpdateDonateFormReqModel request)
         {
+            var userId = _jwtService.decodeToken(token, "userId");
+            var user = await _unitOfWork.Users.GetByIdAsync(userId);
+            if (user == null)
+                throw new ApiException(HttpStatusCode.NotFound, "User not found.");
+
             var donation = await _unitOfWork.DonateForm.GetByIdAsync(id);
             if (donation == null) throw new ApiException(HttpStatusCode.BadRequest, "Donation not found");
 
-            donation.ItemName = request.ItemName;
-            donation.ItemDescription = request.ItemDescription;
-            donation.DonateQuantity = request.Quantity;           
-
+            donation.Status = request.Status;          
             _unitOfWork.DonateForm.Update(donation);
             await _unitOfWork.SaveAsync();
         }
